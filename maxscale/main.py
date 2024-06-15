5532340c50a37bd7d89b4aa32e6610bb62e0fe6a
luongvv-rtc <luongvv@hotmail.com>
2024-06-14 21:24:24 -0700

    Updated

--- maxscale/main.py
+++ maxscale/main.py
@@ -6,7 +6,28 @@
 
 
 import mysql.connector
+# Connect to Database Servers
+
+def db_connection():
+    return mysql.connector.connect(
+        host="172.23.0.4",
+        port="4000",
+        user="maxuser",
+        password="maxpwd"
+    )
+
+cursor = db.cursor()
+
+# Connect to Zipcodes_One
+print('zipcodes_one DB:')
+cursor.execute("SELECT Zipcode FROM zipcodes_one.zipcodes_one;")
+result = cursor.fetchone()
+print(result[0])
 
 
-
+# Connect to Zipcodes_Two
+print('zipcodes_two DB:')
+cursor.execute("SELECT Zipcode FROM zipcodes_two.zipcodes_two;")
+result = cursor.fetchone()
+print(result[0])
 
