4bdf77346d4a25bf959941417e3330106d54b7d7
luongvv-rtc <luongvv@hotmail.com>
2024-06-14 21:27:19 -0700

    Updated

--- maxscale/main.py
+++ maxscale/main.py
@@ -6,8 +6,8 @@
 
 
 import mysql.connector
-# Connect to Database Servers
 
+# Connect to Database Servers
 def db_connection():
     return mysql.connector.connect(
         host="172.23.0.4",
@@ -16,7 +16,15 @@ def db_connection():
         password="maxpwd"
     )
 
-cursor = db.cursor()
+# Fetch result
+def fetch_results(cursor, query):
+    cursor.execute(query)
+    return [result[0] for result in cursor.fetchall() if result[0] != ""]
+
+# def main
+def main():
+    db = db_connection()
+    cursor = db.cursor()
 
 # Connect to Zipcodes_One
 print('zipcodes_one DB:')
@@ -30,4 +38,8 @@ print('zipcodes_two DB:')
 cursor.execute("SELECT Zipcode FROM zipcodes_two.zipcodes_two;")
 result = cursor.fetchone()
 print(result[0])
+
+
+if __name__ == "__main__":
+    main()
 
