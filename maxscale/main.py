c196e6c0ccf8c66a4b4ac575d74d6c9cc0287134
luongvv-rtc <luongvv@hotmail.com>
2024-06-14 21:28:44 -0700

    Updated

--- maxscale/main.py
+++ maxscale/main.py
@@ -26,18 +26,7 @@ def main():
     db = db_connection()
     cursor = db.cursor()
 
-# Connect to Zipcodes_One
-print('zipcodes_one DB:')
-cursor.execute("SELECT Zipcode FROM zipcodes_one.zipcodes_one;")
-result = cursor.fetchone()
-print(result[0])
 
-
-# Connect to Zipcodes_Two
-print('zipcodes_two DB:')
-cursor.execute("SELECT Zipcode FROM zipcodes_two.zipcodes_two;")
-result = cursor.fetchone()
-print(result[0])
 
 
 if __name__ == "__main__":
